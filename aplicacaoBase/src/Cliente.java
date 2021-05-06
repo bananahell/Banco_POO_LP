7e8895ce27ac89045959b6a23349e819cb68bd20
Pedro Nogueira <cmcg89034@gmail.com>
2021-05-05 23:59:31 -0300

    Limpada inicial nas identações do professor que, por experiência, sabia que estariam zoadas zzzzz

--- aplicacaoBase/src/Cliente.java
+++ aplicacaoBase/src/Cliente.java
@@ -1,36 +1,27 @@
 public class Cliente {
 
-    private String nome;
-    
-    private String cpf;
-    
-    
+  private String nome;
+  private String cpf;
 
-	public Cliente(String cpf, String nome) {
+  public Cliente(String cpf, String nome) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
 
-	        this.nome = nome;
-        	this.cpf = cpf;
-        
-	}
+  public String getCpf() {
+    return cpf;
+  }
 
+  public String getNome() {
+    return nome;
+  }
 
-	public String getCpf() {
-		return cpf;
-	}
+  public void setCpf(String string) {
+    cpf = string;
+  }
 
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public void setCpf(String string) {
-		cpf = string;
-	}
-
-
-	public void setNome(String string) {
-		nome = string;
-	}
+  public void setNome(String string) {
+    nome = string;
+  }
 
 }
--- aplicacaoBase/src/ListaClientes.java
+++ aplicacaoBase/src/ListaClientes.java
@@ -1,32 +1,26 @@
 
 public class ListaClientes {
 
+  private RepositorioClientesArray clientes;
+  private int indiceLista;
 
-		
-		private RepositorioClientesArray clientes;
-		private int indiceLista;
-			
-		public ListaClientes() {
-			clientes = new RepositorioClientesArray();
-			indiceLista = 0;
-		}
-		
-		public boolean hasNext() {
-			
-			return  indiceLista < clientes.getIndice();
-		}
-		
-		public Cliente next () {
-			Cliente c = clientes.getClientes()[indiceLista];
-			indiceLista = indiceLista + 1;
-			return c;
-		}
-		
-		public void insert (Cliente c) {
-			clientes.inserir(c);
-			
-		}
+  public ListaClientes() {
+    clientes = new RepositorioClientesArray();
+    indiceLista = 0;
+  }
 
+  public boolean hasNext() {
+    return indiceLista < clientes.getIndice();
+  }
 
+  public Cliente next() {
+    Cliente c = clientes.getClientes()[indiceLista];
+    indiceLista = indiceLista + 1;
+    return c;
+  }
+
+  public void insert(Cliente c) {
+    clientes.inserir(c);
+  }
 
 }
